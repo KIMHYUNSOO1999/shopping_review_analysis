363e4bd1ccc197b1529364cc09e2a90345241a08
Hyun Soo Kim <93048764+KIMHYUNSOO1999@users.noreply.github.com>
2023-03-03 01:16:57 +0900

    2023-03-02 koBERT모델

---
+++ Processing/processing_BERT.py
@@ -0,0 +1,160 @@
+!pip install mxnet
+!pip install gluonnlp pandas tqdm
+!pip install sentencepiece
+!pip install transformers==3.0.2
+!pip install torch
+!pip install git+https://git@github.com/SKTBrain/KoBERT.git@master
+
+# torch
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import pandas as pd
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+
+#kobert
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+
+#GPU 사용
+device = torch.device("cuda:0")
+
+#BERT 모델, Vocabulary 불러오기 필수
+bertmodel, vocab = get_pytorch_kobert_model()
+
+
+# KoBERT에 입력될 데이터셋 정리
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))  
+
+# 모델 정의
+class BERTClassifier(nn.Module): ## 클래스를 상속
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes=2,  
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+# Setting parameters
+max_len = 80
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 5
+max_grad_norm = 1
+log_interval = 200
+learning_rate =  5e-5
+
+## 학습 모델 로드
+PATH = '너 파일경로'
+model = torch.load(PATH + 'KoBERT.pt')  # 전체 모델을 통째로 불러옴, 클래스 선언 필수
+model.load_state_dict(torch.load(PATH + 'model_state_dict.pt'))  # state_dict를 불러 온 후, 모델에 저장
+
+#토큰화
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+
+def predict(df):
+
+    test_eval=[]
+
+    another_test = BERTDataset(df, 0, 1, tok, max_len, True, False)
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
+    
+    model.eval()
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length= valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+
+        
+        for i in out:
+            logits=i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append(0)
+            elif np.argmax(logits) == 1:
+                test_eval.append(1)
+          
+    return test_eval
+
+
+def exam():
+    df=pd.read_csv("/content/neg.csv")
+    
+    del df['Unnamed: 0']
+
+    df['review'] = df['review'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+    df['review'].replace('', np.nan, inplace=True)
+
+    df['review'].nunique()
+    df.drop_duplicates(subset=['review'], inplace=True)
+    
+    data_list=[]
+    
+    for i in df['review']:
+        data = []
+        data.append(str(i))
+        data.append(0)
+
+    data_list.append(data)
+    
+    test_eval=[]
+    
+    result=predict(data_list)
+    
+    data = {
+    'text':df['review'],
+    'label':result
+    }
+ 
+    df_final = pd.DataFrame(data)
+    
+    df_final.to_csv('/content/neg_label.csv',encoding='CP949')
+            
+exam()
